9c4761bc9a88467c45fdc032732edb08e03d37b2
caiohnrq <caio_henrique.16@hotmail.com>
2026-01-07 00:24:09 -0300

    feat: consumindo o DB do techchallenge-12SOAT-db

--- tf/dev/backend.tf
+++ tf/dev/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "techchallenge-12soat-tfstate-bucket-us1"
-    key            = "dev/eks-cluster/terraform.tfstate"
+    bucket         = "techchallenge-soat12-db-state-db"
+    key            = "rds/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock-dynamo"
-    encrypt        = true
   }
 }
